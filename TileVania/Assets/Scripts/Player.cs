3dc13e5fea76589327a2e81f29d81dd08c773ead
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-07-02 15:16:55 -0400

    Character Movement
    -Character faces direction of movement

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    Rigidbody2D myRigidBody;
+    private void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+}
