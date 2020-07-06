1e428a7b95f9185d32628c8551b1e7e491253d4c
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-07-06 13:45:08 -0400

    FIxed spam jumping

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -9,10 +9,12 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpSpeed = 5f;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
     private void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D=GetComponent<Collider2D>();
     }
     private void Update()
     {
@@ -33,11 +35,16 @@ public class Player : MonoBehaviour
     }
     void Jump()
     {
+        if(!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd;
         }
+  
     }
     void FlipSprite()
     {
