1ae70e163aa9b920a51eefc3d0f349ce99472d33
Eddie Marrero Jr <50213623+eddmarre@users.noreply.github.com>
2020-07-23 14:40:40 -0400

    Added Risisng water

---
+++ TileVania/Assets/Scripts/VerticalScrol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScrol : MonoBehaviour
+{
+    [Tooltip("Game Units Per Second")]
+    [SerializeField] float scrollRate = .2f;
+
+    private void Update()
+    {
+        float yMove = scrollRate * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+    }
+}
